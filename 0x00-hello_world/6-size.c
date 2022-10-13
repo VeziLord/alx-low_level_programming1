0e547e896b4666ce25596bcd77e873fd868501e7
VeziLord <chigozi2008@gmail.com>
2022-10-13 16:24:30 -0700

    C task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main = Prints many var types
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizof(b));
+	printf("Size of lonf int: %dbyte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d yte(s)\n",  sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	return (0);
+}
+
+
+
+
+
+
+
+
+			'
