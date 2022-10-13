d6a1acd4b65d186f34789355d5a67ec9b9834a55
VeziLord <chigozi2008@gmail.com>
2022-10-13 15:42:50 -0700

    C task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main = Print in a text
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
