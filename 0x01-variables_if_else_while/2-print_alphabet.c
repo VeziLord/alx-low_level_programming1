8bada3f82362b145b609e515aa1878f4b727d3ba
VeziLord <chigozi2008@gmail.com>
2022-10-17 12:06:54 -0700

    C V2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main = Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+
+		}
+	putchar("\n");
+	return (0);
+
+}
