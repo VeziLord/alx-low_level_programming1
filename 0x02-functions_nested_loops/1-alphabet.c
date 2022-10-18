9cee079a4efd0367846a1b817baef26af2bb34c0
VeziLord <chigozi2008@gmail.com>
2022-10-18 10:49:32 -0700

    vezi nested 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * Print_alphabet = Print all alphabet in lowercase
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
