a3ef3e8ecf3431b8a3970584826587b30a65a5e0
VeziLord <chigozi2008@gmail.com>
2022-10-18 12:00:18 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,12 @@
 /**
  * print_alphabet = print all alphabet in lowercase
  */
-void print_alphabet_x10(void);
+void print_alphabet(void);
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	-putchar('\n');
 }
