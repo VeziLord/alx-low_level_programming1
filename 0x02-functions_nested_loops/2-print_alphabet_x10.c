79fc8a2029f2977b51695b43cf5d5d6ba2aa8918
VeziLord <chigozi2008@gmail.com>
2022-10-19 03:19:08 -0700

    vezi nested 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabeth 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int num;
+	char c;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (c - 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
