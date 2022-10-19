42457ed6fd674096538afc1a896ef3263af4ba4e
VeziLord <chigozi2008@gmail.com>
2022-10-19 16:04:46 -0700

    vezi loop 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - print every minute of the day of Jack Bauer
+ * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
@@ -11,12 +11,12 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar((hour / 10) = '0');
-			_putchar((HOUR % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
-}		
+}
