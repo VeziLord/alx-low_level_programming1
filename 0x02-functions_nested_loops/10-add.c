fe85922a07df37e0a4fa99b15794d714195a5852
VeziLord <chigozi2008@gmail.com>
2022-10-19 16:32:23 -0700

    vezi loop 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @x: the first number
+ * @y: the second number
+ * Return: the addition of both
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
