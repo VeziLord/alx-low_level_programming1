da5b6acc954ade92e28ddd8134e88eb69a713e0e
VeziLord <chigozi2008@gmail.com>
2022-10-18 21:39:43 -0700

    vezi T1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "main.h"
 /**
- * main = print alphabet in lowercase
+ * print_alphabet - print alphabet in lowercase
+ *
+ * Return: void
  */
-int  print_alphabet(void)
+void print_alphabet(void)
 {
-	char letter;
+	int letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
+	}
 	_putchar('\n');
+
 }
