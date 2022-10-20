383096ab786cf45926dd5b8333c136971bb136fa
VeziLord <chigozi2008@gmail.com>
2022-10-20 09:21:18 -0700

    debug vezi 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - causing an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 *
+	 * }
+	 */
+	printf("infinite loop avoided! \\o/\n");
+	return (0);
+}
