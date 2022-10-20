37e25c3c05a04d2474f4c7b157344a8cf72896f0
VeziLord <chigozi2008@gmail.com>
2022-10-20 11:28:04 -0700

    vezi bug 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - causing an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -12,11 +13,12 @@ int main(void)
 
 	i = 0;
 	/*
-	 * while (i < 10)
+	 * *while (i < 10)
 	 * {
 	 *
 	 * }
 	 */
 	printf("infinite loop avoided! \\o/\n");
+
 	return (0);
 }
